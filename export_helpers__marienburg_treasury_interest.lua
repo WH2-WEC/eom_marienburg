054c32770193fe5b92bcc399c14c1b407c4360ad
DrunkFlamingo <matte901@gmail.com>
2018-07-30 17:55:13 -0400

    added logging to interst script

--- export_helpers__marienburg_treasury_interest.lua
+++ export_helpers__marienburg_treasury_interest.lua
@@ -1,3 +1,4 @@
+events = get_events(); cm = get_cm(); marien = _G.marien
 local marienburg_interest = {
     cap = 5000,
     rate = (5/100),
@@ -7,32 +8,36 @@ local marienburg_interest = {
 
 --v function(marienburg: CA_FACTION)
 local function marienburg_evaluate_interest(marienburg)
-if marienburg:has_technology("tech_Marien_Banking_1") then
-    marienburg_interest.rate = (8/100)
-end
-if marienburg:has_technology("tech_Marien_Banking_2") then
-    marienburg_interest.cap = 10000
-end
-if marienburg:has_technology("tech_Marien_Banking_3a") then
-    marienburg_interest.cap = 20000
-end
+    marien:log("Checking interest this turn for ["..marienburg:name().."] ")
+    if marienburg:has_technology("tech_Marien_Banking_1") then
+        marienburg_interest.rate = (8/100)
+    end
+    if marienburg:has_technology("tech_Marien_Banking_2") then
+        marienburg_interest.cap = 10000
+    end
+    if marienburg:has_technology("tech_Marien_Banking_3a") then
+        marienburg_interest.cap = 20000
+    end
 
-local treasury = marienburg:treasury()
-local interest = treasury * marienburg_interest.rate
-local rounded_interest = math.floor((interest/100) + 0.5) * 100
-local interest_bundle = marienburg_interest.bundle_prefix..rounded_interest
-local old_bundle = cm:get_saved_value("sam_marienburg_interest")
-if old_bundle == nil then
-    cm:set_saved_value("sam_marienburg_interest", interest_bundle)
-    cm:apply_effect_bundle(interest_bundle, marienburg:name(), 0)
-    return
-end
-
-if not old_bundle == interest_bundle then
-    cm:remove_effect_bundle(old_bundle, marienburg:name())
-    cm:apply_effect_bundle(interest_bundle, marienburg:name(), 0)
-    cm:set_saved_value("sam_marienburg_interest", interest_bundle)
-end
+    local treasury = marienburg:treasury()
+    local interest = treasury * marienburg_interest.rate
+    local rounded_interest = math.floor((interest/100) + 0.5) * 100
+    local interest_bundle = marienburg_interest.bundle_prefix..rounded_interest
+    marien:log("new Interest bundle should be ["..interest_bundle.."] ")
+    local old_bundle = cm:get_saved_value("sam_marienburg_interest")
+    if old_bundle == nil then
+        marien:log("setting the bundle for the first time!")
+        cm:set_saved_value("sam_marienburg_interest", interest_bundle)
+        cm:apply_effect_bundle(interest_bundle, marienburg:name(), 0)
+        return
+    end
+    marien:log("old_bundle was ["..old_bundle.."] ")
+    if not old_bundle == interest_bundle then
+        cm:remove_effect_bundle(old_bundle, marienburg:name())
+        cm:apply_effect_bundle(interest_bundle, marienburg:name(), 0)
+        cm:set_saved_value("sam_marienburg_interest", interest_bundle)
+        marien:log("applied new interest bundle!")
+    end
 end
 
 
