31afae00ce9e2598a576622d86a2a2828586ead1
DrunkFlamingo <matte901@gmail.com>
2018-08-03 16:42:21 -0400

    fixed scope bug

--- export_helpers__marienburg_mutual_protection.lua
+++ export_helpers__marienburg_mutual_protection.lua
@@ -1,6 +1,6 @@
-local BRET_PROTECTION_BUNDLE = "SAM_ADD" 
-local EMP_PROTECTION_BUNDLE = "SAM_ADD"
-local PROTECTION_TECH = "SAM_ADD"
+SAM_BRET_PROTECTION_BUNDLE = "marien_emp_positive_relations" 
+SAM_EMP_PROTECTION_BUNDLE = "marien_bret_positive_relations"
+SAM_PROTECTION_TECH = "tech_Marien_Diplomacy_1"
 
 --v function(subculture: string) --> boolean
 local function marienburg_at_war_with(subculture)
@@ -23,12 +23,12 @@ core:add_listener(
         return context:recipient():name() == "wh_main_emp_marienburg"
     end,
     function(context)
-        if cm:get_faction("wh_main_emp_marienburg"):has_technology(PROTECTION_TECH) or MARIENBURG_TESTING_VAR == true then
+        if cm:get_faction("wh_main_emp_marienburg"):has_technology(SAM_PROTECTION_TECH) or MARIENBURG_TESTING_VAR == true then
             if context:proposer():subculture() == "wh_main_sc_emp_empire" then
-                cm:apply_effect_bundle(EMP_PROTECTION_BUNDLE, "wh_main_emp_marienburg", 0)
+                cm:apply_effect_bundle(SAM_EMP_PROTECTION_BUNDLE, "wh_main_emp_marienburg", 0)
                 marien:log("War declared on marienburg by ["..context:proposer():name().."] applying the empire war bundle!")
             elseif context:proposer():subculture() == "wh_main_sc_brt_bretonnia" then
-                cm:apply_effect_bundle(BRET_PROTECTION_BUNDLE, "wh_main_emp_marienburg", 0)
+                cm:apply_effect_bundle(SAM_BRET_PROTECTION_BUNDLE, "wh_main_emp_marienburg", 0)
                 marien:log("War declared on marienburg by ["..context:proposer():name().."] applying the bret war bundle!")
             end
         end
@@ -41,17 +41,20 @@ core:add_listener(
     "FactionTurnStart",
     true,
     function(context)
+        if cm:model():turn_number() == 2 and MARIENBURG_TESTING_VAR == true then
+            cm:force_declare_war("wh_main_emp_empire", "wh_main_emp_marienburg", false, false)
+        end
         local marienburg = cm:get_faction("wh_main_emp_marienburg")
         if not marienburg_at_war_with("wh_main_sc_emp_empire") then
-            if marienburg:has_effect_bundle(EMP_PROTECTION_BUNDLE) then
+            if marienburg:has_effect_bundle(SAM_EMP_PROTECTION_BUNDLE) then
                 marien:log("Not at war with Empire anymore, removing the protection bundle!")
-                cm:remove_effect_bundle(EMP_PROTECTION_BUNDLE, marienburg:name())
+                cm:remove_effect_bundle(SAM_EMP_PROTECTION_BUNDLE, marienburg:name())
             end
         end
         if not marienburg_at_war_with("wh_main_sc_brt_bretonnia") then
-            if marienburg:has_effect_bundle(BRET_PROTECTION_BUNDLE) then
+            if marienburg:has_effect_bundle(SAM_BRET_PROTECTION_BUNDLE) then
                 marien:log("Not at war with bretonnia anymore, removing the protection bundle!")
-                cm:remove_effect_bundle(BRET_PROTECTION_BUNDLE, marienburg:name())
+                cm:remove_effect_bundle(SAM_BRET_PROTECTION_BUNDLE, marienburg:name())
             end
         end
     end,
