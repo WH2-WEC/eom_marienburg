a14e341fb223cee00395355d49e0c7e471bff3f2
DrunkFlamingo <matte901@gmail.com>
2018-08-19 09:18:06 -0400

    finalized faction unity

--- script/export_helpers__marienburg_faction_unity.lua
+++ script/export_helpers__marienburg_faction_unity.lua
@@ -9,46 +9,46 @@ SAM_MARIENBURG_POST_TECH_UNITY_SUCCESS_CHANCE = 35
 
 SAM_MARIENBURG_BATTLE_VALUE = 1
 SAM_MARIENBURG_SETTLEMENT_EMPIRE_VALUES = {
-        ["wh_main_ostland_castle_von_rauken"] = 2,
-        ["wh_main_ostland_norden"] = 2,
-        ["wh_main_ostland_wolfenburg"] = 2,
+        ["wh_main_ostland_castle_von_rauken"] = 1,
+        ["wh_main_ostland_norden"] = 1,
+        ["wh_main_ostland_wolfenburg"] = 1,
         ["wh_main_reikland_altdorf"] = 20,
-        ["wh_main_reikland_eilhart"] = 2,
-        ["wh_main_reikland_grunburg"] = 2,
-        ["wh_main_reikland_helmgart"] = 2,
-        ["wh_main_stirland_the_moot"] = 2,
-        ["wh_main_stirland_wurtbad"] = 2,
-        ["wh_main_talabecland_kemperbad"] = 2,
-        ["wh_main_wissenland_nuln"] = 2,
-        ["wh_main_wissenland_pfeildorf"] = 2,
-        ["wh_main_wissenland_wissenburg"] = 2,
-        ["wh_main_hochland_brass_keep"] = 2,
-        ["wh_main_hochland_hergig"] = 2,
-        ["wh_main_middenland_carroburg"] = 2,
+        ["wh_main_reikland_eilhart"] = 1,
+        ["wh_main_reikland_grunburg"] = 1,
+        ["wh_main_reikland_helmgart"] = 1,
+        ["wh_main_stirland_the_moot"] = 1,
+        ["wh_main_stirland_wurtbad"] = 1,
+        ["wh_main_talabecland_kemperbad"] = 1,
+        ["wh_main_wissenland_nuln"] = 1,
+        ["wh_main_wissenland_pfeildorf"] = 1,
+        ["wh_main_wissenland_wissenburg"] = 1,
+        ["wh_main_hochland_brass_keep"] = 1,
+        ["wh_main_hochland_hergig"] = 1,
+        ["wh_main_middenland_carroburg"] = 1,
         ["wh_main_middenland_middenheim"] = 15,
-        ["wh_main_middenland_weismund"] = 2,
-        ["wh_main_nordland_dietershafen"] = 2,
-        ["wh_main_nordland_salzenmund"] = 2,
-        ["wh_main_talabecland_talabheim"] = 2,
-        ["wh_main_averland_averheim"] = 2,
-        ["wh_main_averland_grenzstadt"] = 2,
-        ["wh_main_ostermark_bechafen"] = 2,
-        ["wh_main_ostermark_essen"] = 2
+        ["wh_main_middenland_weismund"] = 1,
+        ["wh_main_nordland_dietershafen"] = 1,
+        ["wh_main_nordland_salzenmund"] = 1,
+        ["wh_main_talabecland_talabheim"] = 1,
+        ["wh_main_averland_averheim"] = 1,
+        ["wh_main_averland_grenzstadt"] = 1,
+        ["wh_main_ostermark_bechafen"] = 1,
+        ["wh_main_ostermark_essen"] = 1
     }--:map<string, number>
 SAM_MARIENBURG_SETTLEMENT_BRET_VALUES = {
-        ["wh_main_bastonne_et_montfort_castle_bastonne"] = 2,
-        ["wh_main_bastonne_et_montfort_montfort"] = 2,
-        ["wh_main_bordeleaux_et_aquitaine_aquitaine"] = 2,
-        ["wh_main_bordeleaux_et_aquitaine_bordeleaux"] = 2,
-        ["wh_main_carcassone_et_brionne_brionne"] = 2,
-        ["wh_main_carcassone_et_brionne_castle_carcassonne"] = 2,
-        ["wh_main_couronne_et_languille_couronne"] = 15,
-        ["wh_main_couronne_et_languille_languille"] = 2,
-        ["wh_main_forest_of_arden_castle_artois"] = 2,
-        ["wh_main_forest_of_arden_gisoreux"] = 2,
-        ["wh_main_lyonesse_lyonesse"] = 2,
-        ["wh_main_parravon_et_quenelles_parravon"] = 2,
-        ["wh_main_parravon_et_quenelles_quenelles"] = 2
+        ["wh_main_bastonne_et_montfort_castle_bastonne"] = 1,
+        ["wh_main_bastonne_et_montfort_montfort"] = 1,
+        ["wh_main_bordeleaux_et_aquitaine_aquitaine"] = 1,
+        ["wh_main_bordeleaux_et_aquitaine_bordeleaux"] = 1,
+        ["wh_main_carcassone_et_brionne_brionne"] = 1,
+        ["wh_main_carcassone_et_brionne_castle_carcassonne"] = 1,
+        ["wh_main_couronne_et_languille_couronne"] = 20,
+        ["wh_main_couronne_et_languille_languille"] = 1,
+        ["wh_main_forest_of_arden_castle_artois"] = 1,
+        ["wh_main_forest_of_arden_gisoreux"] = 1,
+        ["wh_main_lyonesse_lyonesse"] = 1,
+        ["wh_main_parravon_et_quenelles_parravon"] = 1,
+        ["wh_main_parravon_et_quenelles_quenelles"] = 1
     }--:map<string, number>
 if cm:get_saved_value("SAM_MARIENBURG_UNITY_BRET") == nil or cm:get_saved_value("SAM_MARIENBURG_UNITY_EMP") == nil then
     cm:set_saved_value("SAM_MARIENBURG_UNITY_BRET", 0)
@@ -61,11 +61,13 @@ local function marienburg_took_settlement(region_name)
         local old_unity = cm:get_saved_value("SAM_MARIENBURG_UNITY_EMP")
         local new_unity = old_unity + SAM_MARIENBURG_SETTLEMENT_EMPIRE_VALUES[region_name]
         cm:set_saved_value("SAM_MARIENBURG_UNITY_EMP", new_unity)
+        marien:log("Marienburg took the settlement ["..region_name.."] and unity is now ["..new_unity.."] ")
     end
     if not not SAM_MARIENBURG_SETTLEMENT_BRET_VALUES[region_name] then
         local old_unity = cm:get_saved_value("SAM_MARIENBURG_UNITY_BRET")
         local new_unity = old_unity + SAM_MARIENBURG_SETTLEMENT_BRET_VALUES[region_name]
         cm:set_saved_value("SAM_MARIENBURG_UNITY_BRET", new_unity)
+        marien:log("Marienburg took the settlement ["..region_name.."] and unity is now ["..new_unity.."] ")
     end
 end
 
@@ -74,6 +76,7 @@ local function marienburg_defeats_empire()
     local old_unity = cm:get_saved_value("SAM_MARIENBURG_UNITY_EMP")
     local new_unity = old_unity + 1
     cm:set_saved_value("SAM_MARIENBURG_UNITY_EMP", new_unity)
+    marien:log("Marienburg defeated the empire and unity is now ["..new_unity.."] ")
 end
 
 --v function()
@@ -81,6 +84,7 @@ local function marienburg_defeats_bret()
     local old_unity = cm:get_saved_value("SAM_MARIENBURG_UNITY_BRET")
     local new_unity = old_unity + 1
     cm:set_saved_value("SAM_MARIENBURG_UNITY_BRET", new_unity)
+    marien:log("Marienburg defeated the bret and unity is now ["..new_unity.."] ")
 end
 
 
@@ -138,6 +142,7 @@ core:add_listener(
     end,
     function(context)
         if not cm:get_saved_value("SAM_MARIENBURG_BRET_UNITY_OCCURED") then
+            marien:log("unity has not occured yet for bret!")
             if cm:get_saved_value("SAM_MARIENBURG_UNITY_BRET") >= SAM_MARIENBURG_BRET_POINT_THRESHOLD then
                 local leader = cm:get_faction("wh_main_brt_bretonnia")
                 local others = leader:factions_of_same_subculture()
@@ -164,6 +169,7 @@ core:add_listener(
             end
         end
         if not cm:get_saved_value("SAM_MARIENBURG_EMP_UNITY_OCCURED") then
+            marien:log("unity has not occured yet for empire!")
             if cm:get_saved_value("SAM_MARIENBURG_UNITY_EMP") >= SAM_MARIENBURG_EMP_POINT_THRESHOLD then
                 local leader = cm:get_faction("wh_main_emp_empire")
                 local others = leader:factions_of_same_subculture()
