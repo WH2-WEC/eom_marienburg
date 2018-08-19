ce9c4889ab3b6be7099bf4e13f2759dee3b9d5ee
DrunkFlamingo <matte901@gmail.com>
2018-08-19 07:56:12 -0400

    added sam's content

--- script/export_helpers__marienburg_mercenary_army.lua
+++ script/export_helpers__marienburg_mercenary_army.lua
@@ -1,18 +1,18 @@
-SAM_MARIENBURG_NORSCA_ARMY_TECH = "SAM_ADD"
+SAM_MARIENBURG_NORSCA_ARMY_TECH = "tech_Marien_Diplomacy_3b"
 SAM_MARIENBURG_NORSCA_ARMY_COOLDOWN = 40
 --button image paths.
-SAM_MARIENBURG_BUTTON_ACTIVE = "SAM_ADD.png" --:string
-SAM_MARIENBURG_BUTTON_DISABLED = "SAM_ADD.png" --:string
-SAM_MARIENBURG_BUTTON_TOOLTIP_ACTIVE = "SAM_ADD" --:string
-SAM_MARIENBURG_BUTTON_TOOLTIP_DISABLED = "SAM_ADD" --:string
+SAM_MARIENBURG_BUTTON_ACTIVE = "ui/skins/default/icon_marien_norscan_diplomacy.png" --:string
+SAM_MARIENBURG_BUTTON_DISABLED = "ui/skins/default/icon_marien_norscan_diplomacy_2.png" --:string
+SAM_MARIENBURG_BUTTON_TOOLTIP_ACTIVE = "ui/skins/default/icon_marien_norscan_diplomacy.png" --:string
+SAM_MARIENBURG_BUTTON_TOOLTIP_DISABLED = "ui/skins/default/icon_marien_norscan_diplomacy_2.png" --:string
 
 
 --spawn information
-SAM_MARIENBURG_SPAWNED_FORCE = "units,seperated,by,commas,no,spaces,SAM,ADD" --:string
-SAM_MARIENBURG_SPAWNED_GENERAL_SUBTYPE = "SAM_ADD"--:string
+SAM_MARIENBURG_SPAWNED_FORCE = "wh_dlc08_nor_inf_marauder_champions_0,wh_dlc08_nor_inf_marauder_champions_0,wh_dlc08_nor_inf_marauder_spearman_0,wh_dlc08_nor_inf_marauder_spearman_0,wh_dlc08_nor_mon_skinwolves_0,wh_main_nor_cav_marauder_horsemen_0,wh_main_nor_cav_marauder_horsemen_0,wh_dlc08_nor_inf_marauder_hunters_0,wh_dlc08_nor_inf_marauder_hunters_0,wh_dlc08_nor_mon_norscan_ice_trolls_0,wh_dlc08_nor_mon_norscan_ice_trolls_0,wh_main_nor_mon_chaos_warhounds_1,wh_main_nor_mon_chaos_warhounds_1" --:string
+SAM_MARIENBURG_SPAWNED_GENERAL_SUBTYPE = "nor_marauder_chieftain"--:string
 --add as many as you like, it will select the name randomly
-SAM_MARIENBURG_SPAWNED_GENERAL_FORENAME = {"SAM_ADD", "SAM_ADD"} --:vector<string>
-SAM_MARIENBURG_SPAWNED_GENERAL_SURNAME = {"SAM_ADD", "SAM_ADD"}--:vector<string>
+SAM_MARIENBURG_SPAWNED_GENERAL_FORENAME = {"names_name_1002972665", "names_name_1007456194"} --:vector<string>
+SAM_MARIENBURG_SPAWNED_GENERAL_SURNAME = {"names_name_2147356358", "names_name_2147356366"}--:vector<string>
 
 if cm:get_saved_value("MARIENBURG_NORSCANS_CD") == nil then
     cm:set_saved_value("MARIENBURG_NORSCANS_CD", 0)
@@ -49,6 +49,7 @@ core:add_listener(
         return context:trigger() == "SAM_MARIENBURG_SPAWN_NORSCANS"
     end,
     function(context)
+        marien:log("Recieved the UI event to spawn norscans!")
         spawn_mercs()
     end,
     true)
@@ -60,22 +61,26 @@ local function get_merc_button()
     local existingButton = Util.getComponentWithName("SamMarienburgNorscaButton")
     if not not existingButton then
         --# assume existingButton: BUTTON
+        marien:log("Found an existing button: returning it")
         return existingButton
     else
         local component = find_uicomponent(core:get_ui_root(), "button_group_management")
         if not not component then
-            local NorscaButton = Button.new("SamMarienburgNorscaButton", component, "CIRCULAR", "SAM_MARIENBURG_BUTTON_ACTIVE")
+            local NorscaButton = Button.new("SamMarienburgNorscaButton", component, "CIRCULAR", SAM_MARIENBURG_BUTTON_ACTIVE)
             NorscaButton:Resize(69, 69)
+            NorscaButton:SetVisible(true)
+            marien:log("Created a new Norsca Button!")
             return NorscaButton  
         else
             marien:log("ERROR: button generation failed, could not find the parent!")
+            return nil
         end
     end
-    return nil
 end
 
 --v function()
 local function trigger_UI_spawn_norsca()
+    marien:log("Sending the UI event to spawn norscans!")
     local marienburg = cm:get_faction("wh_main_emp_marienburg")
     CampaignUI.TriggerCampaignScriptEvent(marienburg:command_queue_index(), "SAM_MARIENBURG_SPAWN_NORSCANS")
 
@@ -85,16 +90,13 @@ end
 
 --v function(active: boolean, button: BUTTON)
 local function set_merc_button_tooltip(active, button)
-    local uic = button:GetContentComponent()
-    if not not uic then
         if active == true then
-            uic:SetTooltipText(SAM_MARIENBURG_BUTTON_TOOLTIP_ACTIVE)
+            marien:log("Setting up the Norscan button as active!")
             button:RegisterForClick(function() trigger_UI_spawn_norsca()  end)
         else 
-            uic:SetTooltipText(SAM_MARIENBURG_BUTTON_TOOLTIP_DISABLED)
+            marien:log("Setting up the Norscan button as inactive!")
             button:RegisterForClick(function() end)
         end
-    end
 end
 
 
@@ -122,8 +124,11 @@ core:add_listener(
                     set_merc_button_tooltip(true, button)
                 end
             else
+                marien:log("Deleting the button, it isn't marienburg's turn!")
                 get_merc_button():Delete()
             end
+        else
+            marien:log("Not creating the Norsca Button! Marienburg does not have the tech yet!")
         end
     end,
     true
@@ -131,7 +136,7 @@ core:add_listener(
 
 
 
-
+marien:log("Norscan button script is active!")
 
 
 
